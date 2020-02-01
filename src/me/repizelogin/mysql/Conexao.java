b4e17ea45368b84d4922b7bac59903849542d6a3
REPIZE <60532776+byyyxd@users.noreply.github.com>
2020-02-01 01:08:33 -0500

    Update Conexao.java

--- src/me/repizelogin/mysql/Conexao.java
+++ src/me/repizelogin/mysql/Conexao.java
@@ -13,16 +13,16 @@ public class Conexao {
 	
 	public static void open() {
 		
-		String url = "jdbc:mysql://144.217.170.82:3306/s391_VOKEPVP";
-		String user = "u391_rpz9PWe24P";
-		String pass = "7r5gWD183PZJQj9uDpF1UTs4";
+		String url = "";
+		String user = "";
+		String pass = "";
 		
 		try {
 			con = DriverManager.getConnection(url, user, pass);
-			Bukkit.getConsoleSender().sendMessage("§a[REPIZE] Login MySQL (Conectado)!");
+			Bukkit.getConsoleSender().sendMessage("Â§a[REPIZE] Login MySQL (Conectado)!");
 			tabela();
 		} catch (SQLException e) {
-			Bukkit.getConsoleSender().sendMessage("§c[REPIZE] Nao foi possivel se conectar ao login MySQL.");
+			Bukkit.getConsoleSender().sendMessage("Â§c[REPIZE] Nao foi possivel se conectar ao login MySQL.");
 		}
  	}
 	
@@ -30,10 +30,10 @@ public class Conexao {
 		PreparedStatement stm = null;
 		try {
 			stm = con.prepareStatement("create table if not exists logins(UUID varchar(64), senha varchar(16))");
-			Bukkit.getConsoleSender().sendMessage("§a[REPIZE] §aTabela criada com sucesso. (Login)");
+			Bukkit.getConsoleSender().sendMessage("Â§a[REPIZE] Â§aTabela criada com sucesso. (Login)");
 			stm.executeUpdate();
 		} catch (SQLException e) {
-			Bukkit.getConsoleSender().sendMessage("§c[REPIZE] Nao foi possivel criar a tabela. (Login)");
+			Bukkit.getConsoleSender().sendMessage("Â§c[REPIZE] Nao foi possivel criar a tabela. (Login)");
 		}
 	}
 	
